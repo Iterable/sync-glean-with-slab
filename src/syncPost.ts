ebff2e37079a8a6a94876b547d86f8141c7bacb0
Christopher Roussel <christopher.roussel@iterable.com>
2023-01-05 11:23:06 -0600

    refactor: Make syncPost accept a single value

--- src/syncPost.ts
+++ src/syncPost.ts
@@ -3,27 +3,26 @@ import { getPost } from "./slab/index.js";
 import { DataSource } from "./datasource.js";
 import { mapPostToGlean } from './syncPosts.js';
 
-const fetchPost = async () => {
+const fetchPost = async (postId: string) => {
   try {
     console.info('Fetching one post from Slab');
-    const response = await getPost('7hfj9e3e');
-    // console.info("Found post", response);
+    const response = await getPost(postId);
     return response;
   } catch (error: any) {
     console.error('Unable to fetch posts from Slab');
     console.trace(error);
     return;
   }
-}
+};
 
-export const ingestSlabPosts = async (uploadId: string) => {
-  const posts = await fetchPost();
+export const ingestSlabPosts = async (postId: string) => {
+  const post = await fetchPost(postId);
 
-  if (!posts) {
+  if (!post) {
     return;
   }
 
-  await addDocument(mapPostToGlean(posts), DataSource.name);
+  await addDocument(mapPostToGlean(post), DataSource.name);
 
   console.info('Finished single post sync');
-}
+};
